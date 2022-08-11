5a7f0c6282084679c1ff915cd301cfad35b3dd4c
Denis Mishankov <denis@mishankov.dev>
2022-08-11 17:32:35 +0300

    add SYNC_ZOOM_SWITCH (#4)

--- src/sync_notification.py
+++ src/sync_notification.py
@@ -10,11 +10,25 @@ if __name__ == "__main__":
 
     if (config.SYNC_FIRST_WEEK_NUMBER - week_number) % 2 == 0:
         log("Sending notification")
-        requests.post(
-            config.SYNC_DISCORD_WEBHOOK_URL,
-            json={
-                "content": f"<@&{config.SYNC_DISCORD_MENTION_ROLE_ID}> Скоро синхронизация. Сегодня она в ZOOM: {config.SYNC_ZOOM_ROOM}"
-            },
-        )
+        
+        if config.SYNC_ZOOM_SWITCH:
+            log("Send ZOOM room")
+
+            requests.post(
+                config.SYNC_DISCORD_WEBHOOK_URL,
+                json={
+                    "content": f"<@&{config.SYNC_DISCORD_MENTION_ROLE_ID}> Скоро синхронизация. Сегодня она в ZOOM: {config.SYNC_ZOOM_ROOM}"
+                },
+            )
+        else:
+            log("Send Discord channel")
+
+            requests.post(
+                config.SYNC_DISCORD_WEBHOOK_URL,
+                json={
+                    "content": f"<@&{config.SYNC_DISCORD_MENTION_ROLE_ID}> Скоро синхронизация тут: <#{config.SYNC_DISCORD_CHANNEL_ID}>"
+                },
+            )
+        
     else:
         log("Not sending notification")
